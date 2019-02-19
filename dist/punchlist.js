03f1030b708aacb46f816f5de60aa442da4fe9b5
Gabriel Wagner <gabwagner@gmail.com>
2019-02-19 10:34:00 -0300

    First Stable Version

--- dist/punchlist.js
+++ dist/punchlist.js
@@ -1,51 +1,87 @@
+
 ;(function ( $, window, document, undefined ) {
-  var punchList = 'defaultPunchList',
-    defaults = {
+    var punchList = 'defaultPunchList';
+  
+    PunchList.defaults = {
+      /* Title of the Punch List */
       title: "Punch List",
-      apiCall: null,
+      /* Width of the container. If not set, will expand to the size of the parent */
+      width: null,
+      /* RestFULL API CALL. Response must be JSON */
+      fillDataCall: null,
+      /* JSON mapping to convert to standarize items */
+      fillDataTransform: null,
+      /* Handler to be called one a item is added 
+          addItemHandler(itemId, val)  
+            itemId: is the unique id that can be used to return the dom of the object 
+            val: is the task value the new item will have 
+      */
+      addItemHandler: null,
+      /* Handler to be called when an item is removed 
+        removeItemHandler(itemId, itemData)
+          itemId: is the unique id that can be used to return the dom of the object 
+          itemData: is the 'item' data $(#itemId).data('item')
+      */
+      removeItemHandler: null,
+      /* Handler to be called when an item change is status 
+        removeItemHandler(itemId, itemData, checked)
+          itemId: is the unique id that can be used to return the dom of the object 
+          itemData: is the 'item' data $(#itemId).data('item')
+          checked: boolean. It tells if the item has been checked or unchecked
+      */    
+      checkedItemHandler: null,
+      // If set on false will not validate the JSON schema
+      // If JSON is invalid the items will not be added in the list
+      validateJSON: true,
+      /* Template of the container */
       punchListContainerTemplate: ({ title }) => `<div class="punchlist-title">
         <h1><i class="fa fa-check"></i>${title}</h1>
-      </div>
-      <div id="punchlist-items-container">
+        </div>
+        <div id="punchlist-items-container">
         <div id="punchlist-items">          
         </div>
-        <div id="add-item">
+        <div id="punchlist-item-action-add">
           <i class="fa fa-plus"></i>
           Add an Item
         </div>        
-      </div>`,
+      </div>`,    
+      /* Template for the data that will be added in each item */
       punchListDataTemplate: (data) =>  `"${data.name}":"${data.value}"`,      
-      punchListCommentTagsTemplate: (tag) =>  `<b>${tag.title}</b>: ${tag.value}`,
+      /* Template of the tags in the comment */
+      punchListCommentTagsTemplate: (tag) =>  `<b>${tag.name}</b>: ${tag.value}`,
+      /* Template of the comments */
       punchListCommentTemplate: (comment) =>  `<div class="punchlist-comment">
           <div class="punchlist-comment-text">${comment.comment}</div>
-          <div class="punchlist-comment-tag">${comment.tags.map(defaults.punchListCommentTagsTemplate).join(' - ')} ${comment.deletable ? '<i class="fa fa-times-circle comment-action"></i>':''}</div>
+          <div class="punchlist-comment-tag">${(comment.tags) ? comment.tags.map(PunchList.defaults.punchListCommentTagsTemplate).join(' - ') : ''}${comment.deletable ? '<i class="fa fa-times-circle punchlist-item-comment-action-remove"></i>':''}</div>
       </div>`,
-      punchListItemTagsTemplate: (tag) =>  `<div class="punchlist-item-tag"><b>${tag.title}</b>: ${tag.value}</div>`,
-      punchListItemTemplate: (item, index) =>  `<div id="item-${index}" data-item='{${item.data.map(defaults.punchListDataTemplate).join(',')}}'>
+      /* Template of the tags in the item */
+      punchListItemTagsTemplate: (tag) =>  `<div class="punchlist-item-tag"><b>${tag.name}</b>: ${tag.value}</div>`,
+      /* Template of the item of the punch list*/
+      punchListItemTemplate: (item, index) =>  `<div id="item-${index}" data-item='{${(item.data)?item.data.map(PunchList.defaults.punchListDataTemplate).join(','):''}}'>
         <div class="punchlist-item">
           <div class="punchlist-item-label">
             <input type="checkbox" id="punch-item-${index}" ${item.checked ? 'checked': ''}/>
-            <label for="punch-item-${index}" class="punchlist-item-label-text" title="${item.item}">
+            <label for="punch-item-${index}" class="punchlist-item-label-text">
               <i class="fa fa-check"></i> 
               <span class="punchlist-item-label-text-line">
-                <span class="punchlist-item-label-text-data">${item.item}</span>
+                <span class="punchlist-item-label-text-data">${item.task}</span>
               </span>
             </label>
           </div>
           <div class="punchlist-item-action" title="comments">
-            <i class="fa fa-comment"></i>
+            <i class="fa fa-comment punchlist-item-action-comment"></i>
           </div>
           <div class="punchlist-item-action" title="remove">
-            <i class="fa fa-times-circle"></i>
+            <i class="fa fa-times-circle punchlist-item-action-remove"></i>
           </div>
         </div>
         <div class="punchlist-item-tags">
-          ${item.tags.map(defaults.punchListItemTagsTemplate).join('')}
+          ${(item.tags)?item.tags.map(PunchList.defaults.punchListItemTagsTemplate).join(''):''}
         </div>
         <div class="punchlist-comments hidden">
-           ${item.comments.map(defaults.punchListCommentTemplate).join('')}
+           ${(item.comments) ? item.comments.map(PunchList.defaults.punchListCommentTemplate).join(''):''}
         </div>
-        <div class="add-item-comment">
+        <div class="punchlist-item-comment-action-add">
           <i class="fa fa-plus"></i>
           Add Comment
         </div>
@@ -55,8 +91,8 @@
   // Override default with provided options
   function PunchList( element, options ) {    
     this.element = element;
-    this.options = $.extend( {}, defaults, options) ;
-    this._defaults = defaults;
+    this.options = $.extend( {}, PunchList.defaults, options) ;
+    this._defaults = PunchList.defaults;
     this._name = punchList;
     // Index is used to provide unique id to new items
     this._index = 0;
@@ -64,29 +100,115 @@
   }          
   // Initilize function
   // If api call is set, add the provide items from the api call to the list.
-  PunchList.prototype.init = function() {
+  PunchList.prototype.init = function() {    
+    var self = this;
+    // Create main punch-list container, add it to the parent container and add specific class for rendering
     var htmlPunchListContainer = [{title: this.options.title}].map(this.options.punchListContainerTemplate).join('');
     $(this.element).html(htmlPunchListContainer);
     $(this.element).addClass('punchlist-container');
-    var self = this;
-    if(this.options.fillDataCall) {
-      $.getJSON(this.options.fillDataCall, function(items) { 
-        self._index = items.length;
-        self.drawItems(self.standardizeData(items));
-      });
+    if(this.options.width) {
+      $(this.element).width(this.options.width);
     }
+    // Populate the punchlist if api call is added
+    this.populatePunchList();
     /* Adding Add Behaviour */
-    $('#add-item').click(function(){ self.addItem() }); 
+    $('#punchlist-item-action-add').click(function(){ self.addItem() }); 
   }
-  // Transform Data to Punch List Data
-  PunchList.prototype.standardizeData = function(data) {
-    var result = [];
+  // Prepopulate the punchlist with user data
+  PunchList.prototype.populatePunchList = function() {
     var self = this;
+    // Will only be called if the fillDataCall is added
+    if(this.options.fillDataCall) {
+      $.ajax({
+        url: this.options.fillDataCall,
+        dataType: 'json',
+        success: function( data ) {
+          self._index = data.length;
+          // Standarize data in case that information is not as punch list is expecting
+          var standarizeData = self.standardizeData(data);
+          // Draw Tasks into the punchList
+          self.drawItems(standarizeData);
+        },
+        error: function( xhr, status, error ) {
+          var message = (error.message) ? error.message : error;
+          console.error("{PunchList][populatePunchList] Error while retrieving data. Status:'" + status + "' - Message:'" + message + "'");
+        }
+      });        
+    } 
+  }  
+  /* 
+    Transform User Data to Punch List Data
+    Transform JSON-Object:
+    Expected Schema:
+      task      : This is the userdata property where is the description of the task.
+      checked   : This is the userdata property where is a boolean value that inform if the task is complete or not.
+                  If not set all task will be in not complete state.
+      data      : This is an array of key value pair of field. This data will be added in each task item that is added.
+                  This information is send when the task is deteled or change it's complete state.
+                  This property is not required.
+                  KeyValue Pair Properties:
+                    name  ->  The key property that will be added in data.
+                    field ->  The property field in the userdata that will be use as value.
+      tags      : This is an array of key value pair of field. This data will be shown under the task name to add extra 
+                  information that could be need.
+                  This property is not required.
+                  KeyValue Pair Properties:
+                    name    ->  The key property that will be added in data.
+                    field   ->  The property field in the userdata that will be use as value.
+                    convert ->  Conver the value in case that need. Current there is a unique "date".
+                                "date": Convert from String to Date and return localeString of it.
+      comments  : This is an object property. It will contain comments of the task that are inside of the userdata.
+                  This property is not required.
+                  Object Properties:
+                    listField : Property in userdata where the Arrays of Comments are. 
+                    field     : The property inside the array where the proper comment is.
+                    deletable : Property informing if the comments in userdata can be deleted.
+                    tags      : See tags.
+    
+    Example:
+    
+      var jsonTransform = {
+        data: [
+        {
+          field: "id",
+          name: "id"
+        }],
+        task: "item",
+        checked: "index",
+        tags: [ 
+          { 
+            field:"project",
+            name:"Project",
+            convert: null,
+          }, 
+          {
+            field:"datetime",
+            name:"Date",
+            convert: "date", 
+          }
+        ],
+        comments: { 
+          listField: "comments", 
+          field:"comment",
+          deletable: false,
+          tags: [
+            {
+              field: "user",
+              name: "User",
+              convert: null,
+            }
+          ]
+        }
+      }   
+  */
+  PunchList.prototype.standardizeData = function(data) {
+    var self = this;
+    var result = [];
     if(this.options.fillDataTransform) {
       var transform = this.options.fillDataTransform;
       $.map(data, function(item) {
         var newItem = {};
-        newItem.item = item[transform.item];
+        newItem.task = item[transform.task];
         newItem.checked =  item[transform.checked];
         newItem.comments = [];
         newItem.tags = [];
@@ -99,6 +221,7 @@
             newItem.data.push(newData);
           });
         }
+        
         if(transform.tags) {
           $.map(transform.tags, function(tag) {
             var newTag = self.standardizeTag(item, tag);
@@ -126,18 +249,14 @@
         result.push(newItem);
       });
     } else {
-        if(!this.validateData(data)) {
-          console.log("Invalid Data Receive from api call " + this.options.fillDataCall);
-        } else {
-          result = data;
-        }
-    }   
+      result = data;
+    }  
     return result;
   }
   // Function to standarize tag
   PunchList.prototype.standardizeTag = function(item, tag) {
     var newTag = {};
-    newTag.title = tag.title;
+    newTag.name = tag.name;
     switch(tag.convert) {
       case "date":
         newTag.value = new Date(item[tag.field]).toLocaleString();
@@ -148,41 +267,107 @@
     } 
     return newTag;   
   }
-  // Draw a list of item provided from the api call
-  PunchList.prototype.drawItems = function(items) {
-    var htmlPunchListContainer = items.map(this.options.punchListItemTemplate).join('');
+  /*
+    Draw a list of tasks provided using defined templates functions. 
+    Expected schema:
+      task    : Field for the task description. 
+      checked : boolean indicating if the task is done or not.
+      data    : An array of KeyValuePair. This information will be added
+                as data inside the task items and will be sent when the   
+                task is deleted or it state is changed.
+      tags    : An array of KeyValuePair. This information will be shown
+                under the task to provide more context of the task.
+      comments: An array of comments that will be shown under the task.
+                Expected comment schema:
+                  comment   : String with the comment.
+                  deletable : boolean declaring if the comment can be 
+                              deleted.
+                  tags      : An array of KeyValuePair. This information 
+                              will be shown under the task to provide more 
+                              context of the task.
+                              
+    Example:
+    
+    var tasks = [ {
+        task:"Task Description",
+        checked:false,
+        data: [ {
+          name: 'id',
+          value: 'TaskUniqueIDonOthePlatform',
+          },
+          ...
+        ],
+        tags: [ {
+          name: 'User',
+          value: 'Boss',
+          },
+          {
+          name: 'Date',
+          value: 'Yesterday',
+          },
+          ...
+        ],        
+        comments: [ {
+          comment: 'This is my comment',
+          deletable: false,
+          tags: [ {
+          name: 'User',
+          value: 'Boss',
+          },
+          {
+          name: 'Date',
+          value: 'Yesterday',
+          },
+          ...
+          ] },
+        ...
+        ] ,
+        ...
+      },
+      ...
+      ];
+    
+  */
+  PunchList.prototype.drawItems = function(tasks) {
+    var htmlPunchListContainer = tasks.map(this.options.punchListItemTemplate).join('');
     var punchlist_items = $(this.element).find('#punchlist-items');
     punchlist_items.html(htmlPunchListContainer);    
-    /* Adding Behaviour */
+    /* Adding Behaviour for all tasks */
     this.setItemFunctionality($(this.element));
   }
-  // Set the behaviour for the new item. 
+// Set the behaviour for the new item. 
   // Behaviours: 
   //  Remove Item
   //  Expand Comments
   //  Add Comment
+  // Set removeItemHandler
+  // Set checkedItemHandler
   PunchList.prototype.setItemFunctionality = function(item) {
     var self = this;
     
-    item.find('.fa-times-circle').click( function(){
+    item.find('.punchlist-item-action-remove').click( function(){
         var parentItem = $(this).parent().parent().parent();
         if(self.options.removeItemHandler) {
             if(self.options.removeItemHandler(parentItem.attr('id'), parentItem.data('item'))) {
               self.removeItem(parentItem);
             } else {
-              console.warn("Item could not be removed - check handler removeItemHandler");
+              console.warn("[PunchList][setItemFunctionality]Item could not be removed - check handler removeItemHandler");
             }
         } else {
           self.removeItem(parentItem);
         }
     });
     
-    item.find('.fa-comment').click(function(){
+    item.find('.punchlist-item-comment-action-remove').click( function(){
+        $(this).parent().parent().remove();
+    });   
+    
+    item.find('.punchlist-item-action-comment').click(function(){
       var parentItem = $(this).parent().parent().parent();
       $(parentItem).find('.punchlist-comments').toggleClass('hidden');
     });
     
-    item.find('.add-item-comment').click(function(){
+    item.find('.punchlist-item-comment-action-add').click(function(){
       self.addComment($(this).parent());
     });    
     
@@ -190,7 +375,7 @@
         if(self.options.checkedItemHandler) {
             var parentItem = $(this).parent().parent().parent();
             if(!self.options.checkedItemHandler(parentItem.attr('id'), parentItem.data('item'), $(this).prop("checked"))) {
-              console.warn("Item could not change state - check handler checkedItemHandler");
+              console.warn("[PunchList][setItemFunctionality] Item could not change state - check handler checkedItemHandler");
               $(this).prop("checked", !$(this).prop("checked"));
             }
         }
@@ -202,8 +387,8 @@
     var comments = item.find('.punchlist-comments');
     var newComment = {comment:'', deletable:true, tags:[]};
     var now = (new Date()).toLocaleString();
-    newComment.tags.push({title:'User',value:'Me'});
-    newComment.tags.push({title:'Date',value:now});
+    newComment.tags.push({name:'User',value:'Me'});
+    newComment.tags.push({name:'Date',value:now});
     var new_comment_html = $.map([newComment],this.options.punchListCommentTemplate).join('');
     var newItem = $(new_comment_html).prependTo(comments);
     var input = document.createElement('input');
@@ -221,10 +406,9 @@
       var itemComment = input.val();
       var itemCommentLength = itemComment.length;
       if (itemCommentLength > 0) {
-        // TODO: Add new item comment handler
         $(this).parent().html(itemComment);
-        newItem.find('.fa-times-circle').click( function(){
-            newItem.remove();
+        newItem.find('.punchlist-item-comment-action-remove').click( function(){
+          newItem.remove();
         });        
       } else {
         newItem.remove();
@@ -236,7 +420,7 @@
     this._adding = true;
     this._index++;
     var punchlist_items = $(this.element).find('#punchlist-items');
-    var new_item_html = $.map({ [this._index]:{item:'',comments:[], tags:[], data:[]}},this.options.punchListItemTemplate).join('');
+    var new_item_html = $.map({ [this._index]:{task:'',comments:[], tags:[], data:[]}},this.options.punchListItemTemplate).join('');
     var newItem = $(new_item_html).appendTo(punchlist_items);
     newItem.find('.punchlist-item-tags').remove();
     var toAppendInput = newItem.find('.punchlist-item-label-text-data');
@@ -255,7 +439,7 @@
       var todoTitleLength = todoTitle.length;
       if (todoTitleLength > 0) {
         if(self.options.addItemHandler&&!self.options.addItemHandler(newItem.attr('id'), input.val())) {
-          console.warn("Item could not be added - check handler addItemHandler");
+          console.warn("[PunchList][addItem] Item could not be added - check handler addItemHandler");
           newItem.remove();
         } else {
            $(this).parent().html(todoTitle);
